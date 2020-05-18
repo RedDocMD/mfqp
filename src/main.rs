5378c3e447eb292221a22c3a22d3ac288f08bced
Deep Majumder <deep.majumder2019@gmail.com>
2020-05-18 21:48:30 +0530

    Some refactoring to code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use curl::easy::Easy;
 use std::str;
 use json;
 use json::JsonValue;
+use std::io;
 
 struct Paper {
     department: String,
@@ -11,21 +12,32 @@ struct Paper {
     year: String,
 }
 
+impl Paper {
+    pub fn new(department: String, link: String, name: String, semester: String, year: String) -> Self {
+        Paper { department, link, name, semester, year }
+    }
+}
+
 fn main() {
     let data_url: String = String::from("https://qp.metakgp.org/data/data.json");
     println!("Fetching JSON file from {} ...", data_url);
     let json_string = get_json_string(&data_url);
     println!("Fetched JSON file.");
     let parsed = json::parse(&json_string).unwrap();
+    let mut input = String::new();
+    println!("Enter the name of the paper to search");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    input = input.trim().to_string();
     println!("Reading through {} entries ...", parsed.len());
-    interpret_json(&parsed);
+    let mut list = Vec::new();
+    interpret_json(&parsed, &mut list, &input);
 }
 
 fn get_json_string(url: &str) -> String {
     let mut easy = Easy::new();
     easy.url(url).unwrap();
     let mut dst = Vec::new();
-    {   
+    {
         let mut transfer = easy.transfer();
         transfer
             .write_function(|data| {
@@ -38,15 +50,15 @@ fn get_json_string(url: &str) -> String {
     str::from_utf8(&dst).unwrap().to_string()
 }
 
-fn interpret_json(parsed: &JsonValue) {
-    for (index, member) in parsed.members().enumerate() {
-        if index == 2534 {
-            for content in member.entries() {
+fn interpret_json(parsed: &JsonValue, list: &mut Vec<Paper>, input: &str) {
+    for member in parsed.members() {
+        let mut add_to_list = false;
+        for content in member.entries() {
+            if content.0 == "Paper" {
                 let val = match content.1.as_str() {
                     Some(s) => s,
                     None => ""
                 };
-                println!("{}: {}", content.0, val);
             }
         }
     }
