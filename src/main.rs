e26c590a0323a5750591efdca79232d72f6f0837
Deep Majumder <deep.majumder2019@gmail.com>
2020-05-18 19:03:00 +0530

    Boilerplate code for json parsing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use curl::easy::Easy;
 use std::str;
+use json;
 
 fn main() {
     let data_url: String = String::from("https://qp.metakgp.org/data/data.json");
     println!("Request URL: {}", data_url);
     let json_string = get_json_string(&data_url);
     println!("Length of json response: {}", json_string.len());
+    let parsed = json::parse(&json_string).unwrap();
+    println!("Is array: {}", parsed.is_array());
+    println!("Array length: {}", parsed.len());
+    for (index, member) in parsed.members().enumerate() {
+        if index == 1 {
+            for content in member.entries() {
+                let val = match content.1.as_str() {
+                    Some(s) => s,
+                    None => ""
+                };
+                println!("{}: {}", content.0, val);
+            }
+        }
+    }
 }
 
 fn get_json_string(url: &str) -> String {
