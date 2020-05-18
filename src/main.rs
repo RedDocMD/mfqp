a7d5daaa7ee894367c48733bc7f14d75ae8382ce
Deep Majumder <deep.majumder2019@gmail.com>
2020-05-18 20:20:54 +0530

    Introduced new struct Some refactoring of code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use curl::easy::Easy;
 use std::str;
 use json;
+use json::JsonValue;
+
+struct Paper {
+    department: String,
+    link: String,
+    name: String,
+    semester: String,
+    year: String,
+}
 
 fn main() {
     let data_url: String = String::from("https://qp.metakgp.org/data/data.json");
     println!("Fetching JSON file from {} ...", data_url);
     let json_string = get_json_string(&data_url);
     println!("Fetched JSON file.");
-    interpret_json(&json_string);
+    let parsed = json::parse(&json_string).unwrap();
+    println!("Reading through {} entries ...", parsed.len());
+    interpret_json(&parsed);
 }
 
 fn get_json_string(url: &str) -> String {
@@ -27,11 +38,9 @@ fn get_json_string(url: &str) -> String {
     str::from_utf8(&dst).unwrap().to_string()
 }
 
-fn interpret_json(json_string: &str) {
-    let parsed = json::parse(&json_string).unwrap();
-    println!("Reading through {} entries ...", parsed.len());
+fn interpret_json(parsed: &JsonValue) {
     for (index, member) in parsed.members().enumerate() {
-        if index == 1 {
+        if index == 2534 {
             for content in member.entries() {
                 let val = match content.1.as_str() {
                     Some(s) => s,
